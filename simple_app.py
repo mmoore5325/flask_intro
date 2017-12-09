50ff5823060e5c17b7494abeff9d29e0b01f2d92
mike <mmoore5325@gmail.com>
2017-12-08 23:17:59 -0500

    created static folders and extended blocks to deal with them

--- simple_app.py
+++ simple_app.py
@@ -9,7 +9,6 @@ def index(name="Mike"):
   return render_template("index.html", name=name)
 
 @app.route('/add/<int:num1>/<int:num2>')
-
 def add(num1, num2):
   return render_template("add.html", num1=num1, num2=num2)
 
